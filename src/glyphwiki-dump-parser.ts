6bd6e7b8936d714e9bfdb15e5144dfa2c27a8586
Tom Cumming <mail@tomcumming.co.uk>
2016-05-02 22:50:04 +0100

    Dont ignore regex failure

--- src/glyphwiki-dump-parser.ts
+++ src/glyphwiki-dump-parser.ts
@@ -7,14 +7,14 @@ export var definitionRegex: RegExp = /\s*(\S+)\s*\|\s*(\S+)\s*\|\s*(\S+)/;
 
 // Throws an exception if the file could not be read
 // ignoreErrors will ignore parse errors
-export function parseFile(filename: string, ignoreErrors?: boolean): Map<string, Definition> {
+export function parseFile(filename: string, ignoreErrors = false): Map<string, Definition> {
   var fs = require('fs');
   var contents = fs.readFileSync(filename, 'utf8');
 
   return parseString(contents, ignoreErrors);
 }
 
-export function parseString(dump: string, ignoreErrors?: boolean): Map<string, Definition> {
+export function parseString(dump: string, ignoreErrors = false): Map<string, Definition> {
   var lines = dump.split('\n');
 
   var ret = new Map<string, Definition>();
@@ -27,8 +27,12 @@ export function parseString(dump: string, ignoreErrors?: boolean): Map<string, D
       if (index === line.length - 1 && line === '') return;
 
       var match = definitionRegex.exec(line);
-      if (match === null && !ignoreErrors) throw new Error('Failed to parse line:' + (index + 1));
-      else ret.set(match[1], { related: match[2], code: match[3] });
+      if (match === null) {
+        if(!ignoreErrors)
+          throw new Error('Failed to parse line:' + (index + 1));
+      } else {
+        ret.set(match[1], { related: match[2], code: match[3] });
+      } 
     }
   });
 
